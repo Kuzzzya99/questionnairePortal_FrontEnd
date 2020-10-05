5db9cd5c411d80ae459187a8fd385acf79ebc06e
kuzzzya99 <kuzaka247872@gmail.com>
2020-10-05 16:10:55 +0300

    Refactor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  host: "https://questionnaireportal.herokuapp.com/",
+  host: "https://questionnaireportal.herokuapp.com",
   ws: 'ws://questionnaireportal.herokuapp.com/ws'
 };
