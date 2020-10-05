fe6b263085e6b6e68bd507a7f9d5707e9ba7a75e
kuzzzya99 <kuzaka247872@gmail.com>
2020-10-05 14:52:32 +0300

    Refactor

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //Install express server
 const express = require('express');
+
 const path = require('path');
 
 const app = express();
@@ -8,7 +9,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-
   res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
